44d32537f847f9e6cbe3ecc3a905f406080314a4
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-05-25 11:22:46 +0300

    Input and InfoBar commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Chat from './components/Chat/Chat';
+import Chat from './component/Chat/Chat';
 import Join from './components/Join/Join';
 
 import { BrowserRouter as Router, Route }
--- client/src/component/Chat/Chat.jsx
+++ client/src/component/Chat/Chat.jsx
@@ -24,7 +24,7 @@ const Chat = ({ location }) => {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
 
-  const ENDPOINT = "localhost:5000";
+  const ENDPOINT = "0.0.0.0:5000";
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
